c3ef06df2d697bc848bd880d13bcac1decb91328
alireza-bamdad <mkalireza.ir@gmail.com>
2023-07-01 20:50:42 +0330

    js started

---
+++ s19-start-javascript/js/script.js
@@ -0,0 +1,17 @@
+// camel case : welcomeToJavascriptCourse 
+// kebab case : welcome-to-javascript-course
+// pascal case : WelcomeToJavascriptCourse
+// snake case : welcome_to_javascript_course
+// upper snake case : WELCOME_TO_JAVASCRIPT_COURSE
+
+
+alert("welcome to JS")
+alert("test") 
+
+
+console.log("hello world!")
+document.write("<h1>salam-h1</h1>")
+document.write("khodafez")
+document.getElementById("another-section").innerText = "<a href='https://google.com'>google.com</a>"
+document.getElementById("hero-section").innerHTML = '<h1 class="title">salam</h1>';
+
