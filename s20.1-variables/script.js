6b71ad0ba77ad3797f99732c7babef68288e418f
alireza-bamdad <mkalireza.ir@gmail.com>
2023-07-03 21:02:26 +0330

    s20 added

---
+++ s20.1-variables/script.js
@@ -0,0 +1,19 @@
+// console.log("alireza","mohamad","4564157",4+5)
+const a = 550;
+const b = 984;
+const c = "alireza";
+console.log("first: a + b = ", a + b);
+// a = 550; //error
+// console.log("second: a + b = ",a + b)
+
+let x = 6;
+let y = 8;
+console.log("first: x + y = ", x + y);
+x = 7;
+console.log("second: x + y = ", x + y);
+
+const _abA$ = "complex variable name";
+console.log(_abA$);
+
+// var z = 32;
+// z = 66;
---
+++ s20.2-data-types/script.js
@@ -0,0 +1,90 @@
+// number & string
+const a = 45;
+const b = "benz";
+console.log("typeof a is", typeof a); // output: number
+console.log("typeof b is", typeof b); // output: string
+const c = "10";
+const d = "20";
+console.log("typeof c is", typeof c);
+console.log("typeof d is", typeof d);
+console.log(c + d);
+
+const firstName = "alireza";
+const lastName = "mohammadi";
+const space = " ";
+console.log(firstName + " " + lastName + "!");
+
+// boolean
+const confirmed = true;
+const isMale = false;
+console.log(confirmed, isMale);
+console.log("typeof confirmed is", typeof confirmed);
+console.log("typeof isMale is", typeof isMale);
+
+// undefined
+let fullName;
+console.log(fullName);
+
+// null
+let description = null;
+console.log(description);
+
+// Infinity
+console.log(1000 / 0);
+
+// NaN => Not a Number
+console.log('"50000" - 991 = ', "50000" - 991);
+console.log('"50000" + 991 = ', "50000" + 991);
+console.log('"alireza" - 991 = ', "alireza" - 991);
+console.log('"alireza" * 20 = ', "alireza" * 20);
+
+// array
+const users = ["alireza", "maryam", "sima", "saba", "komeyl"];
+const array1 = ["some description...", 154, true, 9 + 10];
+const numbers_array = [1, 3, 5, 7, 9];
+const weekdays = [
+  "شنبه",
+  "یکشنبه",
+  "دوشنبه",
+  "سه شنبه",
+  "چهارشنبه",
+  "پنجشنبه",
+  "جمعه",
+];
+console.log(users);
+console.log(array1);
+console.log(numbers_array);
+console.log(weekdays);
+console.log(users[4]);
+
+// nested arrays
+const nestedArray = ["alireza", [4236, 3256, [true, false, "morteza"]]];
+console.log(nestedArray);
+
+// object
+const user1 = {
+  firstname: "alireza",
+  lastname: "mohamadi",
+  isMale: true,
+};
+const user2 = {
+  firstname: "maryam",
+  lastname: "rezaie",
+  isMale: false,
+};
+console.log(user1);
+const xiaomi_phone = {
+  vazn: "181 gr",
+  andaze: "70*100*20",
+  specialFeatures: [
+    "مجهز به حس‌گر اثرانگشت",
+    "مناسب عکاسی",
+    "مناسب عکاسی سلفی",
+  ],
+  test: {
+    prop1: "value1",
+    prop2: "value2",
+    prop3: true,
+    prop4: [1, 6, 5],
+  },
+};
