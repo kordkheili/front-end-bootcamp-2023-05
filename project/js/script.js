a01c5c8c337f55f992238a8c744e6818de55fafc
Alireza - Bootcamp B <mkalireza.ir@gmail.com>
2023-08-12 21:09:09 +0330

    s37 added

---
+++ project/js/script.js
@@ -0,0 +1,16 @@
+// const video_el = document.querySelector(".bg-video");
+
+// window.addEventListener("resize", (e) => {
+//   videoHandler();
+// });
+// function videoHandler() {
+//   const width = window.innerWidth;
+//   if (width > 888) {
+//     video_el.style.width = "100%";
+//     video_el.style.height = "auto";
+//   } else {
+//     video_el.style.width = "auto";
+//     video_el.style.height = "100%";
+//   }
+// }
+// videoHandler();
