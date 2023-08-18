7abfd3e8ffe64f971aec4461cd53e75a1e2f65a9
Alireza - Bootcamp B <mkalireza.ir@gmail.com>
2023-08-18 21:05:20 +0330

    S40 ADDED

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -14,3 +14,16 @@
 //   }
 // }
 // videoHandler();
+
+
+const openSearchBtn = document.getElementById("open-search-box");
+const closeSearchBtn = document.getElementById("close-search-box");
+const searchBox = document.getElementById("header-search-box");
+const searchBoxInput = document.querySelector("#header-search-box input[type='text']");
+openSearchBtn.addEventListener("click",()=>{
+    searchBox.classList.add("active")
+    searchBoxInput.focus()
+})
+closeSearchBtn.addEventListener("click",()=>{
+    searchBox.classList.remove("active")
+})
